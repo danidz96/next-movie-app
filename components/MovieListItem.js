c7005594389941e9555c3a0a30b51dab479508ba
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-06 00:14:17 +0200

    add shorter description

--- components/MovieListItem.js
+++ components/MovieListItem.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const MovieListItem = ({ movie }) => {
+  const shorten = (text, maxLength) => {
+    if (text && text.length > maxLength) {
+      return text.substr(0, maxLength) + '...';
+    }
+
+    return text;
+  };
+
   return (
     <div className="col-lg-4 col-md-6 mb-4">
       <div className="card h-100">
@@ -12,7 +20,7 @@ const MovieListItem = ({ movie }) => {
             <a href="#">{movie.name}</a>
           </h4>
           <h5>$24.99</h5>
-          <p className="card-text">{movie.description}</p>
+          <p className="card-text">{shorten(movie.description, 100)}</p>
         </div>
         <div className="card-footer">
           <small className="text-muted">{movie.rating}</small>
