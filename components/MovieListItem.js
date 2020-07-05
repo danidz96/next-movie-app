5e6ec19c17b7a475e8aa979992ba08f3e33ab031
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-05 20:45:24 +0200

    create movie list item

---
+++ components/MovieListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const MovieListItem = ({ movie }) => {
+  return (
+    <div className="col-lg-4 col-md-6 mb-4">
+      <div className="card h-100">
+        <a href="#">
+          <img className="card-img-top" src={movie.image} alt="" />
+        </a>
+        <div className="card-body">
+          <h4 className="card-title">
+            <a href="#">{movie.name}</a>
+          </h4>
+          <h5>$24.99</h5>
+          <p className="card-text">{movie.description}</p>
+        </div>
+        <div className="card-footer">
+          <small className="text-muted">{movie.rating}</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieListItem;
