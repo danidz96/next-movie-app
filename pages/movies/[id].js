46a13223e6a3385933e1adc9358ccd74f99d995f
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-08 00:12:11 +0200

    update movie detail page

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -7,17 +7,21 @@ const Movie = ({ movie }) => {
     <div className="container">
       <div className="jumbotron">
         <h1 className="display-4">{movie.name}</h1>
-        <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content
-          or information.
-        </p>
+        <p className="lead">{movie.description}</p>
         <hr className="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p>{movie.genre}</p>
         <a className="btn btn-primary btn-lg" href="#" role="button">
           Learn more
         </a>
       </div>
-      <p>Some description about the movie</p>
+      <p className="desc-text">{movie.longDesc}</p>
+      <style jsx>
+        {`
+          .desc-text {
+            font-size: 21px;
+          }
+        `}
+      </style>
     </div>
   );
 };
