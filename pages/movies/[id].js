93c22625469ef4d21048bef2fbbd1dc09d24333e
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-07 23:26:25 +0200

    create movies page

---
+++ pages/movies/[id].js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Movie = () => {
+  const { query } = useRouter();
+  return (
+    <div className="container">
+      <h1>movie with id: {query.id}</h1>
+    </div>
+  );
+};
+
+export default Movie;
