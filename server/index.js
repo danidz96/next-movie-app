649aa12772e83a4efec45554ececd5ae05a4afea
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-14 23:18:59 +0200

    update server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const next = require('next');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -7,6 +8,27 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+
+  server.use(bodyParser.json());
+
+  server.get('/api/v1/movies', (req, res) => {
+    return res.json({ message: 'Hello World' });
+  });
+
+  server.post('/api/v1/movies', (req, res) => {
+    const movie = req.body;
+    return res.json({ ...movie, createdTime: 'today', author: 'Dani' });
+  });
+
+  server.patch('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Updating post of id: ${id}` });
+  });
+
+  server.delete('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Deleting post of id: ${id}` });
+  });
 
   server.get('*', (req, res) => {
     return handle(req, res);
